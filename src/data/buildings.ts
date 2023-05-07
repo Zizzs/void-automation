dc301a3c4fe87a4e21d47a6cc1e37f64664f8993
Zizzs <Zizzs17@gmail.com>
2023-05-07 13:16:34 -0700

    Added initial building data
    Building data init

---
+++ src/data/buildings.ts
@@ -0,0 +1,17 @@
+const buildings = {
+  miner: {
+    one: {
+      name: "Miner",
+      tier: "One",
+      rate: 15,
+    },
+  },
+  constructors: {
+    one: {
+      name: "Constructor",
+      tier: "One",
+      rate: 1,
+      maxInput: 2,
+    },
+  },
+};
